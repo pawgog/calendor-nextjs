e758c2a28fc3ee83447ca1c4edc0cf34a1afe771
pawgog <pgogola10@gmail.com>
2025-07-02 17:26:49 +0200

    Set up tailwind configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="text-center container my-4 mx-auto">
       <h1 className="text-3xl mb-4">Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
+      </div>
     </div>
   );
 }

cddb5f45a901739ae04b7995676e30b54bd6be3e
pawgog <pgogola10@gmail.com>
2025-07-12 12:16:29 +0200

    Set up clerk ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  if (userId != null) redirect("/events");
+
   return (
     <div className="text-center container my-4 mx-auto">
       <h1 className="text-3xl mb-4">Home Page</h1>
       <div className="flex gap-2 justify-center">
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
       </div>
     </div>
   );

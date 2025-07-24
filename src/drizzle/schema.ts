a88b0a035526ac0308c9685dc8d03ecacf94c814
pawgog <pgogola10@gmail.com>
2025-07-24 18:03:55 +0200

    Finish db schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -2,17 +2,22 @@ import {
   boolean,
   index,
   integer,
+  pgEnum,
   pgTable,
   text,
   timestamp,
   uuid,
 } from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants"
 
 const createdAt = timestamp("createdAt").notNull().defaultNow()
 const updatedAt = timestamp("updatedAt")
   .notNull()
   .defaultNow()
   .$onUpdate(() => new Date())
+
+export const scheduleDayOfWeekEnum = pgEnum("day", DAYS_OF_WEEK_IN_ORDER)
 
 export const EventTable = pgTable(
   "events",
@@ -28,5 +33,43 @@ export const EventTable = pgTable(
   },
   table => ({
     clerkUserIdIndex: index("clerkUserIdIndex").on(table.clerkUserId),
+  })
+)
+
+export const ScheduleTable = pgTable("schedules", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  timezone: text("timezone").notNull(),
+  clerkUserId: text("clerkUserId").notNull().unique(),
+  createdAt,
+  updatedAt,
+})
+
+export const scheduleRelations = relations(ScheduleTable, ({ many }) => ({
+  availabilities: many(ScheduleAvailabilityTable),
+}))
+
+export const ScheduleAvailabilityTable = pgTable(
+  "scheduleAvailabilities",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    scheduleId: uuid("scheduleId")
+      .notNull()
+      .references(() => ScheduleTable.id, { onDelete: "cascade" }),
+    startTime: text("startTime").notNull(),
+    endTime: text("endTime").notNull(),
+    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(),
+  },
+  table => ({
+    scheduleIdIndex: index("scheduleIdIndex").on(table.scheduleId),
+  })
+)
+
+export const ScheduleAvailabilityRelations = relations(
+  ScheduleAvailabilityTable,
+  ({ one }) => ({
+    schedule: one(ScheduleTable, {
+      fields: [ScheduleAvailabilityTable.scheduleId],
+      references: [ScheduleTable.id],
+    }),
   })
 )

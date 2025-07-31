e44e68c969403a62d97883514410311fa2dd07b0
pawgog <pgogola10@gmail.com>
2025-07-31 21:03:49 +0200

    Create events schema and form request

--- src/components/forms/EventForm.tsx
+++ src/components/forms/EventForm.tsx
@@ -1,3 +1,15 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { eventFormSchema } from "@/schema/events";
+
 export function EventForm() {
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: {
+      isActive: true,
+      durationInMinutes: 30,
+    },
+  });
   return null;
 }

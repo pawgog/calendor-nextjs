674fe8600e9e1007e880ac21bee70a667a7edbb8
pawgog <pgogola10@gmail.com>
2025-08-26 18:35:12 +0200

    Improve navigation layout style

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -6,19 +6,19 @@ import { NavLink } from "@/components/NavLink";
 export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <header className="flex py-2 border-b bg-card">
-        <nav className="font-medium flex items-center text-sm gap-6 container">
-          <div className="flex items-center gap-2 font-semibold mr-auto">
+      <header className="flex justify-center py-2 border-b bg-card">
+        <nav className="font-medium flex justify-between items-center text-sm container">
+          <div className="flex items-center gap-2 font-semibold">
             <CalendarRange className="size-6" />
             <span className="sr-only md:not-sr-only">Calendor</span>
           </div>
-          <NavLink href="/events">Events</NavLink>
-          <NavLink href="/schedule">Schedule</NavLink>
-          <div className="ml-auto size-10">
-            <UserButton
-              appearance={{ elements: { userButtonAvatarBox: "size-full" } }}
-            />
+          <div className="flex items-center gap-6 font-semibold">
+            <NavLink href="/events">Events</NavLink>
+            <NavLink href="/schedule">Schedule</NavLink>
           </div>
+          <UserButton
+            appearance={{ elements: { userButtonAvatarBox: "size-full" } }}
+          />
         </nav>
       </header>
       <main className="container my-6 mx-auto">{children}</main>

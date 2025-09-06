42fbb84bc68f7b8dd0b56d9e3038c63673da0814
pawgog <pgogola10@gmail.com>
2025-09-06 11:03:54 +0200

    Set up schedule form component and schema

---
+++ src/components/forms/ScheduleForm.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants";
+import { scheduleFormSchema } from "@/schema/schedule";
+import { timeToInt } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { formatTimezoneOffset } from "@/lib/formatters";
+
+type Availability = {
+  startTime: string;
+  endTime: string;
+  dayOfWeek: (typeof DAYS_OF_WEEK_IN_ORDER)[number];
+};
+
+export function ScheduleForm({
+  schedule,
+}: {
+  schedule?: {
+    timezone: string;
+    availabilities: Availability[];
+  };
+}) {
+  const form = useForm<z.infer<typeof scheduleFormSchema>>({
+    resolver: zodResolver(scheduleFormSchema),
+    defaultValues: {
+      timezone:
+        schedule?.timezone ?? Intl.DateTimeFormat().resolvedOptions().timeZone,
+      availabilities: schedule?.availabilities.toSorted((a, b) => {
+        return timeToInt(a.startTime) - timeToInt(b.startTime);
+      }),
+    },
+  });
+
+  async function onSubmit() {
+    return null;
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex gap-6 flex-col"
+      >
+        {form.formState.errors.root && (
+          <div className="text-destructive text-sm">
+            {form.formState.errors.root.message}
+          </div>
+        )}
+        <FormField
+          control={form.control}
+          name="timezone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Timezone</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {Intl.supportedValuesOf("timeZone").map((timezone) => (
+                    <SelectItem key={timezone} value={timezone}>
+                      {timezone}
+                      {` (${formatTimezoneOffset(timezone)})`}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-2 justify-end">
+          <Button disabled={form.formState.isSubmitting} type="submit">
+            Save
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}

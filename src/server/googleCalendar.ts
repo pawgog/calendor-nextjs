0872281eb827a2bdab6864cdcacf405747043fa2
pawgog <pgogola10@gmail.com>
2025-10-12 11:48:54 +0200

    Add create calendar event

--- src/server/googleCalendar.ts
+++ src/server/googleCalendar.ts
@@ -2,7 +2,58 @@ import "use-server"
 
 import { clerkClient } from "@clerk/nextjs/server"
 import { google } from "googleapis"
-import { endOfDay, startOfDay } from "date-fns"
+import { addMinutes, endOfDay, startOfDay } from "date-fns"
+
+export async function createCalendarEvent({
+  clerkUserId,
+  guestName,
+  guestEmail,
+  startTime,
+  guestNotes,
+  durationInMinutes,
+  eventName,
+}: {
+  clerkUserId: string
+  guestName: string
+  guestEmail: string
+  startTime: Date
+  guestNotes?: string | null
+  durationInMinutes: number
+  eventName: string
+}) {
+  const oAuthClient = await getOAuthClient(clerkUserId)
+  const clerk = await clerkClient();
+  const calendarUser = await clerk.users.getUser(clerkUserId)
+  if (calendarUser.primaryEmailAddress == null) {
+    throw new Error("Clerk user has no email")
+  }
+
+  const calendarEvent = await google.calendar("v3").events.insert({
+    calendarId: "primary",
+    auth: oAuthClient,
+    sendUpdates: "all",
+    requestBody: {
+      attendees: [
+        { email: guestEmail, displayName: guestName },
+        {
+          email: calendarUser.primaryEmailAddress.emailAddress,
+          displayName: calendarUser.fullName,
+          responseStatus: "accepted",
+        },
+      ],
+      description: guestNotes ? `Additional Details: ${guestNotes}` : undefined,
+      start: {
+        dateTime: startTime.toISOString(),
+      },
+      end: {
+        dateTime: addMinutes(startTime, durationInMinutes).toISOString(),
+      },
+      summary: `${guestName} + ${calendarUser.fullName}: ${eventName}`,
+    },
+  })
+
+  return calendarEvent.data
+}
 
 export async function getCalendarEventTimes(
   clerkUserId: string,

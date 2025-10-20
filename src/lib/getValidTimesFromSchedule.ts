31ab4e0222a6f522b65010bdda18cc51040b63fa
pawgog <pgogola10@gmail.com>
2025-10-20 16:59:48 +0200

    Create get valid time from schedule function

---
+++ src/lib/getValidTimesFromSchedule.ts
@@ -0,0 +1,59 @@
+import { db } from "@/drizzle/db"
+import { getCalendarEventTimes } from "@/server/googleCalendar"
+import {
+  addMinutes,
+  areIntervalsOverlapping,
+  isWithinInterval,
+} from "date-fns"
+
+export async function getValidTimesFromSchedule(
+  timesInOrder: Date[],
+  event: { clerkUserId: string; durationInMinutes: number }
+) {
+  const start = timesInOrder[0]
+  const end = timesInOrder.at(-1)
+
+  if (start == null || end == null) return []
+
+  const schedule = await db.query.ScheduleTable.findFirst({
+    where: ({ clerkUserId: userIdCol }, { eq }) =>
+      eq(userIdCol, event.clerkUserId),
+    with: { availabilities: true },
+  })
+
+  if (schedule == null) return []
+
+  const groupedAvailabilities = Object.groupBy(
+    schedule.availabilities,
+    a => a.dayOfWeek
+  )
+
+  const eventTimes = await getCalendarEventTimes(event.clerkUserId, {
+    start,
+    end,
+  })
+
+  return timesInOrder.filter(intervalDate => {
+    const availabilities = getAvailabilities(
+      groupedAvailabilities,
+      intervalDate,
+      schedule.timezone
+    )
+    const eventInterval = {
+      start: intervalDate,
+      end: addMinutes(intervalDate, event.durationInMinutes),
+    }
+
+    return (
+      eventTimes.every(eventTime => {
+        return !areIntervalsOverlapping(eventTime, eventInterval)
+      }) &&
+      availabilities.some(availability => {
+        return (
+          isWithinInterval(eventInterval.start, availability) &&
+          isWithinInterval(eventInterval.end, availability)
+        )
+      })
+    )
+  })
+}

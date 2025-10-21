87efa2be63abe62e975573d8d617c72f4c47f3e4
pawgog <pgogola10@gmail.com>
2025-10-21 21:01:32 +0200

    Improved get valid time from schedule function

--- src/lib/getValidTimesFromSchedule.ts
+++ src/lib/getValidTimesFromSchedule.ts
@@ -1,10 +1,24 @@
+import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants"
 import { db } from "@/drizzle/db"
-import { getCalendarEventTimes } from "@/server/googleCalendar"
+import { ScheduleAvailabilityTable } from "@/drizzle/schema"
+
 import {
   addMinutes,
   areIntervalsOverlapping,
+  isFriday,
+  isMonday,
+  isSaturday,
+  isSunday,
+  isThursday,
+  isTuesday,
+  isWednesday,
   isWithinInterval,
+  setHours,
+  setMinutes,
 } from "date-fns"
+import { fromZonedTime } from "date-fns-tz"
+
+import { getCalendarEventTimes } from "@/server/googleCalendar"
 
 export async function getValidTimesFromSchedule(
   timesInOrder: Date[],
@@ -55,5 +69,64 @@ export async function getValidTimesFromSchedule(
         )
       })
     )
+  })
+}
+
+function getAvailabilities(
+  groupedAvailabilities: Partial<
+    Record<
+      (typeof DAYS_OF_WEEK_IN_ORDER)[number],
+      (typeof ScheduleAvailabilityTable.$inferSelect)[]
+    >
+  >,
+  date: Date,
+  timezone: string
+) {
+  let availabilities:
+    | (typeof ScheduleAvailabilityTable.$inferSelect)[]
+    | undefined
+
+  if (isMonday(date)) {
+    availabilities = groupedAvailabilities.monday
+  }
+  if (isTuesday(date)) {
+    availabilities = groupedAvailabilities.tuesday
+  }
+  if (isWednesday(date)) {
+    availabilities = groupedAvailabilities.wednesday
+  }
+  if (isThursday(date)) {
+    availabilities = groupedAvailabilities.thursday
+  }
+  if (isFriday(date)) {
+    availabilities = groupedAvailabilities.friday
+  }
+  if (isSaturday(date)) {
+    availabilities = groupedAvailabilities.saturday
+  }
+  if (isSunday(date)) {
+    availabilities = groupedAvailabilities.sunday
+  }
+
+  if (availabilities == null) return []
+
+  return availabilities.map(({ startTime, endTime }) => {
+    const start = fromZonedTime(
+      setMinutes(
+        setHours(date, parseInt(startTime.split(":")[0])),
+        parseInt(startTime.split(":")[1])
+      ),
+      timezone
+    )
+
+    const end = fromZonedTime(
+      setMinutes(
+        setHours(date, parseInt(endTime.split(":")[0])),
+        parseInt(endTime.split(":")[1])
+      ),
+      timezone
+    )
+
+    return { start, end }
   })
 }

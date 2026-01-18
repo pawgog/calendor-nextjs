928ae24473d33e854c6ee18fb79bd64ad46aba49
pawgog <pgogola10@gmail.com>
2026-01-18 14:21:19 +0100

    Add new navigation link

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,20 +1,30 @@
 import { ReactNode } from "react";
 import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { CalendarRange } from "lucide-react";
 import { NavLink } from "@/components/NavLink";
 
-export default function PrivateLayout({ children }: { children: ReactNode }) {
+export default async function PrivateLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (userId == null) return redirectToSignIn();
+
   return (
     <>
       <header className="flex justify-center py-2 border-b bg-card">
         <nav className="font-medium flex justify-between items-center text-sm container">
           <div className="flex items-center gap-2 font-semibold">
-            <CalendarRange className="size-6" />
-            <span className="sr-only md:not-sr-only">Calendor</span>
+            <CalendarRange className="size-6 text-indigo-500" />
+            <span className="sr-only md:not-sr-only">Calendling</span>
           </div>
           <div className="flex items-center gap-6 font-semibold">
             <NavLink href="/events">Events</NavLink>
-            <NavLink href="/schedule">Schedule</NavLink>
+            <NavLink href="/availability">Set availability</NavLink>
+            <NavLink href={`/book/${userId}`}>My Schedule</NavLink>
           </div>
           <UserButton
             appearance={{ elements: { userButtonAvatarBox: "size-full" } }}
